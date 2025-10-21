3f57ef2e8af51784cbc32e9d4e93833f728fdb17
DmitriyShilyaev <goodrus2010@yandex.ru>
2025-10-21 21:59:13 +0500

    Выполнение задачи седьмого спринта: 3 этап

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -7,16 +7,16 @@
 
 import UIKit
 
-class AlertPresenter: AlertPresenterProtocol {
+final class AlertPresenter: AlertPresenterProtocol {
     func makeResultsMessage(correctAnswers: Int, totalQuestions: Int, gamesCount: Int, bestGame: GameResult, totalAccuracy: Double) -> String {
         let dateString = bestGame.date.dateTimeString
-            return """
-            Ваш результат: \(correctAnswers)/\(totalQuestions)
-            Количество сыгранных квизов: \(gamesCount)
-            Рекорд: \(bestGame.correct)/\(bestGame.total) (\(dateString))
-            Средняя точность: \(String(format: "%.2f", totalAccuracy))%
-            """
-        }
+        return """
+        Ваш результат: \(correctAnswers)/\(totalQuestions)
+        Количество сыгранных квизов: \(gamesCount)
+        Рекорд: \(bestGame.correct)/\(bestGame.total) (\(dateString))
+        Средняя точность: \(String(format: "%.2f", totalAccuracy))%
+        """
+    }
     
     func show(in viewController: UIViewController, model: AlertModel) {
         let alert = UIAlertController(title: model.title, message: model.message, preferredStyle: .alert)
@@ -24,7 +24,6 @@ class AlertPresenter: AlertPresenterProtocol {
         alert.addAction(UIAlertAction(title: model.buttonText, style: .default) { _ in
             model.completion()
         })
-        viewController.present(alert, animated: true, completion: nil)
+        viewController.present(alert, animated: true)
     }
 }
-    
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-final class MovieQuizViewController: UIViewController {
+final class MovieQuizViewController: UIViewController, MovieQuizViewControllerProtocol {
     
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
@@ -9,98 +9,94 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var noButton: UIButton!
     @IBOutlet private var activityIndicator: UIActivityIndicatorView!
     
-    //private var correctAnswers = 0
-    
     private var presenter: MovieQuizPresenter!
-
-        // MARK: - Lifecycle
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-
-            presenter = MovieQuizPresenter(viewController: self)
-
-            imageView.layer.cornerRadius = 20
-        }
-
-        // MARK: - Actions
-
-        @IBAction private func yesButtonClicked(_ sender: UIButton) {
-           
-            presenter.yesButtonClicked()
-        }
-
-        @IBAction private func noButtonClicked(_ sender: UIButton) {
-           
-            presenter.noButtonClicked()
-        }
-
-        // MARK: - Private functions
-
-        func show(quiz step: QuizStepViewModel) {
-
-            imageView.layer.borderColor = UIColor.clear.cgColor
-            imageView.image = step.image
-            textLabel.text = step.question
-            counterLabel.text = step.questionNumber
-        }
-
-        func show(quiz result: QuizResultsViewModel) {
-            let message = presenter.makeResultsMessage()
-            
-
-            let alert = UIAlertController(
-                title: result.title,
-                message: message,
-                preferredStyle: .alert)
-                alert.view.accessibilityIdentifier = "Game results"
-
-                let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
-                    guard let self = self else { return }
-
-                    self.presenter.restartGame()
-                }
-
-            alert.addAction(action)
-
-            self.present(alert, animated: true, completion: nil)
-        }
-
-        func highlightImageBorder(isCorrectAnswer: Bool) {
-            imageView.layer.masksToBounds = true
-            imageView.layer.borderWidth = 8
-            imageView.layer.borderColor = isCorrectAnswer ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-        }
-
-        func showLoadingIndicator() {
-            activityIndicator.isHidden = false // говорим, что индикатор загрузки не скрыт
-            activityIndicator.startAnimating() // включаем анимацию
-        }
-
-        func hideLoadingIndicator() {
-            activityIndicator.isHidden = true
-        }
-
-        func showNetworkError(message: String) {
-            hideLoadingIndicator()
-
-            let alert = UIAlertController(
-                title: "Ошибка",
-                message: message,
-                preferredStyle: .alert)
-
-                let action = UIAlertAction(title: "Попробовать ещё раз",
-                style: .default) { [weak self] _ in
-                    guard let self = self else { return }
-
-                    self.presenter.restartGame()
-                }
-
-            alert.addAction(action)
-        }
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        presenter = MovieQuizPresenter(viewController: self)
+        
+        imageView.layer.cornerRadius = 20
     }
+    
+    // MARK: - Actions
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        
+        presenter.yesButtonClicked()
+    }
+    
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        
+        presenter.noButtonClicked()
+    }
+    
+    // MARK: - Private functions
+    
+    func show(quiz step: QuizStepViewModel) {
+        
+        imageView.layer.borderColor = UIColor.clear.cgColor
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
+    
+    func show(quiz result: QuizResultsViewModel) {
+        let message = presenter.makeResultsMessage()
+        
+        
+        let alert = UIAlertController(
+            title: result.title,
+            message: message,
+            preferredStyle: .alert)
+        alert.view.accessibilityIdentifier = "Game results"
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            
+            self.presenter.restartGame()
+        }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func highlightImageBorder(isCorrectAnswer: Bool) {
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrectAnswer ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+    }
+    
+    func showLoadingIndicator() {
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+    }
+    
+    func hideLoadingIndicator() {
+        activityIndicator.isHidden = true
+    }
+    
+    func showNetworkError(message: String) {
+        hideLoadingIndicator()
+        let alert = UIAlertController(
+            title: "Ошибка",
+            message: message,
+            preferredStyle: .alert)
+        alert.view.accessibilityIdentifier = "Network Error"
+        let retryAction = UIAlertAction(title: "Попробовать ещё раз", style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            self.showLoadingIndicator()
+            self.presenter.reloadData()
+        }
+        alert.addAction(retryAction)
+        present(alert, animated: true)
+    }
+}
 
-/*
+ /*
  Mock-данные
  
  
