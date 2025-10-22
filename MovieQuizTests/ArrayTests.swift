56996493348845b1dac6f2493d0c7290b9843794
DmitriyShilyaev <goodrus2010@yandex.ru>
2025-10-22 17:15:23 +0500

    test: add arrayTests

--- MovieQuizTests/ArrayTests.swift
+++ MovieQuizTests/ArrayTests.swift
@@ -9,10 +9,27 @@ import XCTest
 @testable import MovieQuiz
 
 class ArrayTests: XCTestCase {
-    func testGetValueInRange() throws {
+    func testGetValueInRange() throws { // тест на успешное взятие элемента по индексу
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 2]
+        
+        // Then
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 2)
     }
     
     func testGetValueOutOfRange() throws {
-    }
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+        // When
+        let value = array[safe: 20]
+        
+        // Then
+        XCTAssertNil(value)
+    } 
 }
 
